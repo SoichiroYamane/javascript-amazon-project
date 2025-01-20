f68bbd961e86448cf0e411ca9d4a9c3aba7e724f
Soichiro Yamane <soichiro.yamane.19970619@gmail.com>
2025-01-20 14:46:15 +0900

    ✨ (amazon.html): add external script for product data to separate concerns ♻️ (amazon.js): remove inline product data and fix typo in rating property

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,40 +1,10 @@
-const products = [
-  {
-    image: "athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      starts: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      starts: 4.0,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      starts: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
 let productHTML = "";
 
 products.forEach((product) => {
   const html = `
       <div class="product-container">
         <div class="product-image-container">
-          <img class="product-image" src="images/products/${product.image}">
+          <img class="product-image" src="${product.image}">
         </div>
 
         <div class="product-name limit-text-to-2-lines">
@@ -42,7 +12,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-rating-container">
-          <img class="product-rating-stars" src="images/ratings/rating-${product.rating.starts * 10}.png">
+          <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
           <div class="product-rating-count link-primary">
             ${product.rating.count}
           </div>
