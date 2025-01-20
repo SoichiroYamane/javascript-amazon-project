66a8c3eccabe14e98a353d0f2137fd536a981d76
Soichiro Yamane <soichiro.yamane.19970619@gmail.com>
2025-01-20 14:40:10 +0900

    init commit and generate HTML with JavaScript

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+// import pluginJs from "@eslint/js";
+
+export default [
+  {
+    files: ["**/*.js"],
+    rules: {
+      semi: "error",
+      // "no-unused-vars": "error"
+    },
+  },
+];
