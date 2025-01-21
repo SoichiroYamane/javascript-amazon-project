25cfbf7efa47e121d48958fbdec90babe295e929
Soichiro Yamane <soichiro.yamane.19970619@gmail.com>
2025-01-21 15:08:49 +0900

    ♻️ (orderSummary.js): remove unused delivery date calculation to clean up code

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -8,11 +8,6 @@ import {
 } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 
-const today = dayjs();
-const deliveryDate = today.add(7, "days");
-deliveryDate.format("dddd, MMMM D");
-console.log(deliveryDate.format("dddd, MMMM D"));
-
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
   cart.forEach((cartItem) => {
