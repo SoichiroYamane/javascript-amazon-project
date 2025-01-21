9aa78d738e31fbb274c13bc15dd6856abf09041e
Soichiro Yamane <soichiro.yamane.19970619@gmail.com>
2025-01-21 14:19:49 +0900

    ✨ (cart.js, checkout.js): add updateDeliveryOption function to allow users to select delivery options for cart items

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
@@ -94,7 +94,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         ? "FREE"
         : `$${formatCurrency(deliveryOption.priceCents)} -`;
     html += `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
       <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}" ${isChecked ? "checked" : ""}>
       <div>
         <div class="delivery-option-date">
@@ -120,5 +120,13 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
       `.js-cart-item-container-${productId}`,
     );
     container.remove();
+  });
+});
+
+document.querySelectorAll(".js-delivery-option").forEach((element) => {
+  element.addEventListener("click", () => {
+    const productId = element.dataset.productId;
+    const deliveryOptionId = element.dataset.deliveryOptionId;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });
